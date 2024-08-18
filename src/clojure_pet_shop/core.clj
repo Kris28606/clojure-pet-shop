83f3ba55d5f4127b208d64e13bd238c514e53430
Kristina Stanisavljevic <stanisavljevickristina28@gmail.com>
2024-08-19 01:30:31 +0200

    Added ring, composure and korma for db

--- src/clojure_pet_shop/core.clj
+++ src/clojure_pet_shop/core.clj
@@ -1,7 +1,29 @@
 (ns clojure-pet-shop.core
-  (:gen-class))
+  (:require [compojure.api.sweet :refer :all]
+            [ring.util.http-response :refer :all]
+            [schema.core :as s]
+            [clojure-pet-shop.domain :refer :all]
+            [clojure-pet-shop.query :refer :all]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def app
+  (api
+   {:swagger
+    {:ui "/"
+     :spec "/swagger.json"
+     :data {:info {:title "PetShop API"
+                   :description "API for a college project made with clojure"}
+            :tags [{:name "users", :description "users api."}]}}}
+
+   (context "/users" []
+     :tags ["users"]
+
+     (GET "/" []
+       :return [Korisnik]
+       :summary "Gets all users"
+       (ok (get-users)))
+
+     (POST "/" []
+       :summary "Creates a new user"
+       :body [newKorisnik NewKorisnik]
+       (ok (add-user newKorisnik))))))
+
